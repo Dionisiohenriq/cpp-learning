bbd9ee75df871a8d06bd5c4b672bc85f9f87bc7a
Henrique Dionisio Ferreira <henriquedionisio.dionisioferre@gmail.com>
2020-08-14 02:01:58 -0300

    primeiroCodigo

---
+++ PrimeiroCodigo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	/* O programa imprime "EU VOU APRENDER C++" em diversas linhas, tando com std::endl
+	 quanto com \n*/
+	// Henrique
+	std::cout << "EU" << std::endl;
+	std::cout << "VOU"<< std::endl;
+	std::cout << "APRENDER \n";
+	std::cout << "C++" << std::endl;
+	system("COLOR f0");
+	system("PAUSE");
+	
+	return 0;
+}
